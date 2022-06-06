7cd9ffbbf1fe0c71bfa21b60a2f9520a939c316f
ZonalDean <huber.deanc@gmail.com>
2022-06-06 15:25:32 +0700

    Setup Complete

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config;
+const express = require("express");
+const cors = require('cors')
+const {sequelize} = require('sequelize')
+const morgan = require('morgan')
+
+// BASICS
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// IMPORTS
+
+const NotFound = require('./middlewares/NotFound');
+const customError = require('./middlewares/customError');
+
+// MIDDLEWARE IMPORT
+
+// ROUTES
+
+// ERROR HANDLING
+app.use(customError)
+app.use(NotFound)
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+});
